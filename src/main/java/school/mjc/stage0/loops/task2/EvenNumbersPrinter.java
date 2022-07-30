0fda26e1677a3b47caa255368a520cc0f9fec68b
komronz <pandacoder0820@gmail.com>
2022-07-30 21:46:07 +0500

    done

--- src/main/java/school/mjc/stage0/loops/task2/EvenNumbersPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task2/EvenNumbersPrinter.java
@@ -2,12 +2,10 @@ package school.mjc.stage0.loops.task2;
 
 public class EvenNumbersPrinter {
     public void printEvenNumbers(int printTillInclusive) {
-        int counter = 0;
-        while (counter < printTillInclusive) {
-            counter++;
-            if (counter%2 ==0){
-                System.out.println(counter);
-            }
+        for (int i = 1; i <= printTillInclusive; i++) {
+            if (i%2==0)
+            System.out.println(i);
+
         }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/NaturalNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/NaturalNumbers.java
@@ -2,10 +2,8 @@ package school.mjc.stage0.loops.task2;
 
 public class NaturalNumbers {
     public void naturalNumbersPrinter(int lastPrinted) {
-        int counter = -1;
-        while (counter < lastPrinted) {
-            counter++;
-            System.out.println(counter);
+        for (int i = 0; i <= lastPrinted; i++) {
+            System.out.println(i);
         }
     }
 }
