fe18e188a76eed3e545ea49cf146c9c12fbf6133
komronz <pandacoder0820@gmail.com>
2022-07-30 21:32:49 +0500

    done

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,6 +2,13 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
+        int a=1;
+        int i = 0;
+        System.out.println(1);
+        while (i < printToInclusive) {
+            i++;
+            System.out.println(a*=i);
+        }
 
     }
 }
